ed83d1319bfa7cc06ff334f3c2183fb803975bfb
CatClientApp <appcat05@gmail.com>
2024-10-29 15:12:38 +0300

    garena button

--- src/components/balance/balance.jsx
+++ src/components/balance/balance.jsx
@@ -3,9 +3,10 @@ import { useTonWallet, useTonConnectModal, useTonConnectUI } from '@tonconnect/u
 import './balance.sass';
 import BalanceBalance from '../balance/balanceBalance/BalanceBalance';
 import TransactionHistory from './transactionHistory/TransactionHistory';
+import { motion } from 'framer-motion';
 
 const Balance = () => {
-	const wallet = 'null';
+	const wallet = null;
 	const [tonConnectUI, setOptions] = useTonConnectUI();
 	const [curPage, setPage] = useState('balance');
 	const [curPageItem, setPageItem] = useState('balance');
@@ -16,10 +17,30 @@ const Balance = () => {
 				<h2 className='title title--22 mb--22'>Balance</h2>
 				<div className='balance__connect'>
 					{wallet === null ? (
-						<button className={`button `} onClick={() => tonConnectUI.openModal().tonConnectUI.openModal()}>
-							Connect Wallet
-						</button>
-					) : (
+						<div className="flex items-center justify-center">
+							<motion.button
+								className="w-auto items-center justify-center px-4 py-1 transform rounded-full font-semibold text-2xl bg-gradient-to-r from-[#0099ff] to-[#00ccff] text-white active:text-black active:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:from-[#00ccff] hover:to-[#0099ff] hover:shadow-lg"
+								style={{
+									boxShadow: '2px 3px 1px #5d5855',
+									border: '1px solid black',
+								}}
+								onClick={() => tonConnectUI.openModal()}
+							>
+								<motion.span
+									className="text-2xl text-white"
+									animate={{
+										opacity: [0.8, 1, 0.8],
+									}}
+									transition={{
+										duration: 1.6,
+										repeat: Infinity,
+										ease: "easeInOut",
+									}}
+								>
+									Connect Wallet
+								</motion.span>
+							</motion.button>
+						</div>) : (
 						<ul className='tabs'>
 							<li
 								className={`tabs__item ${curPageItem === 'balance' ? 'active' : ''}`}
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './fonts.scss';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
